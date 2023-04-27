eb8632c48275472a409670f0e477d28083d0bbea
nalediym <67169576+nalediym@users.noreply.github.com>
2023-04-27 03:09:30 +0000

    initial web scrapper for yahoo finance

---
+++ data_sources/scripts/web_scrappers/yahoo_finance.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://finance.yahoo.com"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+print(soup.find(id="mrt-node-Lead-3-FinanceHeader").text)
