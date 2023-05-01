5ed57e866e6318bf0d0d40d7bc1d4db51b43a4bf
Angela <38588380+achen788@users.noreply.github.com>
2023-05-01 01:56:18 -0400

    updated locations

--- data_sources/scripts/bankfind_suite/locations.py
+++ data_sources/scripts/bankfind_suite/locations.py
@@ -7,17 +7,14 @@ s = requests.Session()
 s.mount("https://", HTTPAdapter(max_retries=10))
 
 locations = pd.DataFrame()
-for state_abbr in ['AK','AL','AR','AS','AZ','CA','CO','CT','DC','DE','FL','FM','GA','GU','HI','IA','ID','IL','IN','KS','KY','LA','MA','MD','ME','MH','MI','MN','MO','MP','MS','MT','NC','ND','NE','NH','NJ','NM','NV','NY','OH','OK','OR','PA','PR','PW','RI','SC','SD','TN','TX','UT','VA','VI','VT','WA','WI','WV','WY']:
-    print(state_abbr, end=' ')
-    if state_abbr=='OR':
-        r = s.get("https://banks.data.fdic.gov/api/locations?filters=STALP:\{}&limit=10000".format(state_abbr))
-    else:
-        r = s.get("https://banks.data.fdic.gov/api/locations?filters=STALP:{}&limit=10000".format(state_abbr))
+total = s.get("https://banks.data.fdic.gov/api/locations").json()['totals']['count']
+limit = 10000
+offset = 0
+while len(locations) < total:
+    r = s.get("https://banks.data.fdic.gov/api/locations?sort_by=ID&offset={}&limit={}".format(offset, limit))
     locations = pd.concat([locations, pd.json_normalize(r.json()['data'])])
-    time.sleep(10)
+    offset += limit
+    time.sleep(5)
 
-if (locations.score==0).all():
-    locations.drop(columns='score',inplace=True)
-locations.columns = [x.replace('data.','') for x in locations.columns if x.startswith('data.')]
-
+locations.columns = [x.replace('data.', '') for x in locations.columns]
 locations.to_csv('../../data/locations.csv', index=False)
