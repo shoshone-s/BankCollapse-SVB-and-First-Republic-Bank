1baac4131739d0e4a1839d5ef16e2a4d1cabedd4
Marks <50840293+Marks20770@users.noreply.github.com>
2023-05-21 17:23:25 -0400

    Updated file name, bucket name and object name to be specified by each user

--- scraper2/aws_access.py
+++ scraper2/aws_access.py
@@ -1,27 +1,30 @@
 import boto3
 import os
-# from pprint import pprint
 import pathlib
 
-# connect to S3
-# s3 = boto3.resource("s3")
 
-# # display bucket names
-# for bucket in s3.buckets.all():
-#     print(bucket.name)
-
-
-# upload file
+# upload file to an S3 bucket
 def upload_file():
     try:
         # uses the boto3 client
         s3 = boto3.client('s3')
-        bucket_name = 'ds4ateam20'
+
+        # specify the S3 bucket name
+        bucket_name = input('Bucket Name: ')
+
         # make sure the object name and file name match
-        object_name = 'svb_debt.csv'
-        file_name = os.path.join(pathlib.Path(
-            __file__).parent.resolve(), 'svb_debt.csv')
-        response = s3.upload_file(file_name, bucket_name, object_name)
+
+        # object_name will prompt in the terminal to specify the object name uploaded to the S3 bucket
+        object_name = input('Object Name: ')
+        # file name is the name that you saved your file under. It will prompt for the file name
+        file_name = input('File Name: ')
+
+        file_name_path = os.path.join(pathlib.Path(
+            __file__).parent.resolve(), file_name)
+
+        # if the file name specified is wrong or not found on the system an error is generated
+
+        response = s3.upload_file(file_name_path, bucket_name, object_name)
         if response == None:
             print("Success")
         else:
