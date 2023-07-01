7f8e8def0b0e6c2b73eb2e5764dd064970d02b39
Angela <38588380+achen788@users.noreply.github.com>
2023-07-01 14:57:00 -0400

    load data to redshift

--- aws_read_write.py
+++ aws_read_write.py
@@ -1,4 +1,5 @@
 import io
+from botocore.exceptions import ClientError
 import boto3
 import pandas as pd
 import configparser
@@ -10,11 +11,6 @@ cfg_data.read("keys_config.cfg")
 AWS_ACCESS_KEY_ID = cfg_data["AWS"]["access_key_id"]
 AWS_SECRET_ACCESS_KEY = cfg_data["AWS"]["secret_access_key"]
 
-S3_BUCKET_NAME = cfg_data["S3"]["bucket_name"]
-REDSHIFT_DB_NAME = cfg_data["Redshift"]["database_name"]
-REDSHIFT_WORKGROUP_NAME = cfg_data["Redshift"]["workgroup_name"]
-IAM_REDSHIFT = cfg_data["Redshift"]["iam_role"]
-
 
 def upload_file(file_name, bucket_name, object_name):
     s3_client = boto3.client(
@@ -23,14 +19,12 @@ def upload_file(file_name, bucket_name, object_name):
         aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
     )
 
-    response = s3_client.upload_file(file_name, bucket_name, object_name)
-
-    status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
-
-    if status == 200:
-        print(f"Successful S3 upload_file response. Status - {status}")
-    else:
-        print(f"Unsuccessful S3 upload_file response. Status - {status}")
+    try:
+        response = s3_client.upload_file(file_name, bucket_name, object_name)
+        print("Successful S3 upload_file response.")
+    except ClientError as e:
+        print("Unsuccessful S3 upload_file response.")
+        logging.error(e)
 
 
 def get_file(bucket_name, object_name):
@@ -71,3 +65,74 @@ def get_parquet(bucket_name, object_name):
     s3_object.download_fileobj(buffer)
     df = pd.read_parquet(buffer)
     return df
+
+
+# execute sql statement
+def execute_sql(region_name, database_name, workgroup_name, sql_statement):
+    print(sql_statement)
+
+    # create Redshift data client
+    redshift_data_client = boto3.client(
+        "redshift-data",
+        region_name=region_name
+    )
+
+    response1 = redshift_data_client.execute_statement(
+        Database=database_name,
+        WorkgroupName=workgroup_name,
+        Sql=sql_statement
+    )
+    # print("response 1\n", response1, "\n")
+
+    response2 = redshift_data_client.describe_statement(Id=response1["Id"])
+
+    # wait for query to finish
+    while response2["Status"] in ["PICKED", "STARTED", "SUBMITTED"]:
+        response2 = redshift_data_client.describe_statement(Id=response1["Id"])
+
+    # if query aborted or failed
+    status = response2["Status"]
+    if status != "FINISHED":
+        print(f"Unsuccessful Redshift Data execute_statement response. Status - {status}")
+        print(response2)
+
+    try:
+        # get query result, status = finished
+        response3 = redshift_data_client.get_statement_result(Id=response1["Id"])
+
+        # return dataframe of result
+        if response3["TotalNumRows"] > 0:
+
+            columns = []
+            for i in range(len(response3["ColumnMetadata"])):
+                columns.append(response3["ColumnMetadata"][i]["name"])
+
+            # list of list of dicts to list of lists (list of row values)
+            result_records = response3["Records"]
+            return pd.DataFrame([[list(d.values())[0] for d in r] for r in result_records], columns=columns)
+
+        else:
+            print("No rows from query")
+    except ClientError as error:
+        print("Query does not have result")
+
+
+# drop table from database
+def drop_table(region_name, database_name, workgroup_name, table_name):
+    sql_statement = f"DROP TABLE IF EXISTS {table_name};"
+    execute_sql(region_name, database_name, workgroup_name, sql_statement)
+
+
+# populate table with data from S3
+def copy_from_s3(region_name, database_name, workgroup_name, table_name, object_path, iam_role):
+    sql_statement = f"""
+        COPY {table_name} 
+        FROM '{object_path}' 
+        IAM_ROLE '{iam_role}' 
+        CSV 
+        DELIMITER ',' 
+        IGNOREHEADER 1 
+        REGION '{region_name}' 
+        ;
+    """
+    execute_sql(region_name, database_name, workgroup_name, sql_statement)
---
+++ redshift_load_data.py
@@ -0,0 +1,105 @@
+import aws_read_write
+import configparser
+
+# read credentials from the config file
+cfg_data = configparser.ConfigParser()
+cfg_data.read("keys_config.cfg")
+
+AWS_ACCESS_KEY_ID = cfg_data["AWS"]["access_key_id"]
+AWS_SECRET_ACCESS_KEY = cfg_data["AWS"]["secret_access_key"]
+
+S3_BUCKET_NAME = cfg_data["S3"]["bucket_name"]
+
+REDSHIFT_DB_NAME = cfg_data["Redshift"]["database_name"]
+REDSHIFT_WORKGROUP_NAME = cfg_data["Redshift"]["workgroup_name"]
+IAM_REDSHIFT = cfg_data["Redshift"]["iam_role"]
+REDSHIFT_REGION_NAME = cfg_data["Redshift"]["region_name"]
+
+
+locations = aws_read_write.get_csv(bucket_name=S3_BUCKET_NAME, object_name='transformed_data/locations.csv')
+
+sql_create_location = """
+CREATE TABLE IF NOT EXISTS location (
+    cert               INTEGER NOT NULL,
+    company_name       VARCHAR(255),
+    main_office        BOOL,
+    branch_name        VARCHAR(255),
+    established_date   DATE,
+    service_type       VARCHAR(255),
+    address            VARCHAR(255),
+    county             VARCHAR(255),
+    city               VARCHAR(255),
+    state              VARCHAR(255),
+    zip                VARCHAR(5),
+    latitude           NUMERIC,
+    longitude          NUMERIC
+);
+"""
+
+aws_read_write.drop_table(
+    region_name=REDSHIFT_REGION_NAME,
+    database_name=REDSHIFT_DB_NAME,
+    workgroup_name=REDSHIFT_WORKGROUP_NAME,
+    table_name="location")
+
+aws_read_write.execute_sql(
+    region_name=REDSHIFT_REGION_NAME,
+    database_name=REDSHIFT_DB_NAME,
+    workgroup_name=REDSHIFT_WORKGROUP_NAME,
+    sql_statement=sql_create_location)
+
+aws_read_write.copy_from_s3(
+    database_name=REDSHIFT_DB_NAME,
+    workgroup_name=REDSHIFT_WORKGROUP_NAME,
+    table_name="location",
+    object_path="s3://ds4ateam20/transformed_data/locations.csv",
+    iam_role=IAM_REDSHIFT,
+    region_name=REDSHIFT_REGION_NAME)
+
+aws_read_write.execute_sql(
+    region_name=REDSHIFT_REGION_NAME,
+    database_name=REDSHIFT_DB_NAME,
+    workgroup_name=REDSHIFT_WORKGROUP_NAME,
+    sql_statement="SELECT COUNT(*) FROM location")
+
+
+price_history = aws_read_write.get_csv(bucket_name=S3_BUCKET_NAME, object_name='transformed_data/price_history.csv')
+
+sql_create_price_history = """
+CREATE TABLE IF NOT EXISTS price_history (
+    symbol          VARCHAR(5) NOT NULL,
+    date            DATE,
+    "open"          NUMERIC,
+    high            NUMERIC,
+    low             NUMERIC,
+    close           NUMERIC,
+    adjusted_close  NUMERIC,
+    volume          INTEGER
+);
+"""
+
+aws_read_write.drop_table(
+    region_name=REDSHIFT_REGION_NAME,
+    database_name=REDSHIFT_DB_NAME,
+    workgroup_name=REDSHIFT_WORKGROUP_NAME,
+    table_name="price_history")
+
+aws_read_write.execute_sql(
+    region_name=REDSHIFT_REGION_NAME,
+    database_name=REDSHIFT_DB_NAME,
+    workgroup_name=REDSHIFT_WORKGROUP_NAME,
+    sql_statement=sql_create_price_history)
+
+aws_read_write.copy_from_s3(
+    database_name=REDSHIFT_DB_NAME,
+    workgroup_name=REDSHIFT_WORKGROUP_NAME,
+    table_name="price_history",
+    object_path="s3://ds4ateam20/transformed_data/price_history.csv",
+    iam_role=IAM_REDSHIFT,
+    region_name=REDSHIFT_REGION_NAME)
+
+aws_read_write.execute_sql(
+    region_name=REDSHIFT_REGION_NAME,
+    database_name=REDSHIFT_DB_NAME,
+    workgroup_name=REDSHIFT_WORKGROUP_NAME,
+    sql_statement="SELECT COUNT(*) FROM price_history")
