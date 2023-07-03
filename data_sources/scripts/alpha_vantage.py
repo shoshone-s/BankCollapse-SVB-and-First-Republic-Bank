282e48f4d9527813518978f05c183d15c66f7478
Angela <38588380+achen788@users.noreply.github.com>
2023-07-03 16:52:01 -0400

    updates

--- data_sources/scripts/alpha_vantage.py
+++ data_sources/scripts/alpha_vantage.py
@@ -66,16 +66,16 @@ stock_prices.columns = [x.split('. ')[-1] for x in stock_prices.columns]
 
 
 # save data to csv
-companies.to_csv(data_path + "\company_overview.csv", index=False)
-incst.to_csv(data_path + "\income_statement.csv", index=False)
+companies.to_csv(data_path + "\\company_overview.csv", index=False)
+incst.to_csv(data_path + "\\income_statement.csv", index=False)
 balsh.to_csv(data_path + "\\balance_sheet.csv", index=False)
-cashflow.to_csv(data_path + "\cash_flow.csv", index=False)
-stock_prices.to_csv(data_path + "\stock_price_daily.csv", index=False)
+cashflow.to_csv(data_path + "\\cash_flow.csv", index=False)
+stock_prices.to_csv(data_path + "\\stock_price_daily.csv", index=False)
 
 
 # upload data to S3 bucket
-aws_read_write.upload_file(file_name=data_path + '\company_overview.csv', bucket_name=S3_BUCKET_NAME, object_name='raw_data/company_overview.csv')
-aws_read_write.upload_file(file_name=data_path + '\income_statement.csv', bucket_name=S3_BUCKET_NAME, object_name='raw_data/income_statement.csv')
-aws_read_write.upload_file(file_name=data_path + '\balance_sheet.csv', bucket_name=S3_BUCKET_NAME, object_name='raw_data/balance_sheet.csv')
-aws_read_write.upload_file(file_name=data_path + '\cash_flow.csv', bucket_name=S3_BUCKET_NAME, object_name='raw_data/cash_flow.csv')
-aws_read_write.upload_file(file_name=data_path + '\stock_price_daily.csv', bucket_name=S3_BUCKET_NAME, object_name='raw_data/stock_price_daily.csv')
+aws_read_write.upload_file(file_name=data_path + '\\company_overview.csv', bucket_name=S3_BUCKET_NAME, object_name='raw_data/company_overview.csv')
+aws_read_write.upload_file(file_name=data_path + '\\income_statement.csv', bucket_name=S3_BUCKET_NAME, object_name='raw_data/income_statement.csv')
+aws_read_write.upload_file(file_name=data_path + '\\balance_sheet.csv', bucket_name=S3_BUCKET_NAME, object_name='raw_data/balance_sheet.csv')
+aws_read_write.upload_file(file_name=data_path + '\\cash_flow.csv', bucket_name=S3_BUCKET_NAME, object_name='raw_data/cash_flow.csv')
+aws_read_write.upload_file(file_name=data_path + '\\stock_price_daily.csv', bucket_name=S3_BUCKET_NAME, object_name='raw_data/stock_price_daily.csv')
--- transform.py
+++ transform.py
@@ -27,8 +27,8 @@ clean_locations['service_type'] = clean_locations['service_type'].replace({11:'F
 clean_locations['zip'] = clean_locations['zip'].astype(str)
 clean_locations.loc[~clean_locations.state.isin(['Puerto Rico','Virgin Islands Of The U.S.']) & clean_locations.zip.apply(lambda x: len(x)!=5), 'zip'] = clean_locations['zip'].str.zfill(5)
 
-clean_locations.to_csv(data_path + "\clean_locations.csv", index=False)
-aws_read_write.upload_file(file_name=data_path + '\clean_locations.csv', bucket_name=S3_BUCKET_NAME, object_name='transformed_data/locations.csv')
+clean_locations.to_csv(data_path + "\\clean_locations.csv", index=False)
+aws_read_write.upload_file(file_name=data_path + '\\clean_locations.csv', bucket_name=S3_BUCKET_NAME, object_name='transformed_data/locations.csv')
 
 
 # combine data from Alpha Vantage, Yahoo Finance, and DJUSBK
@@ -58,5 +58,5 @@ clean_price_history['volume'] = clean_price_history['volume'].astype('Int64')
 
 
 # save data to csv and upload data to S3 bucket
-clean_price_history.to_csv(data_path + "\clean_price_history.csv", index=False)
-aws_read_write.upload_file(file_name=data_path + '\clean_price_history.csv', bucket_name=S3_BUCKET_NAME, object_name='transformed_data/price_history.csv')
+clean_price_history.to_csv(data_path + "\\clean_price_history.csv", index=False)
+aws_read_write.upload_file(file_name=data_path + '\\clean_price_history.csv', bucket_name=S3_BUCKET_NAME, object_name='transformed_data/price_history.csv')
