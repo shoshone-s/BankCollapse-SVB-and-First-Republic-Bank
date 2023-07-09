0ea060e2536a9a35f3a30de040a72c5826e8c54b
nalediym <67169576+nalediym@users.noreply.github.com>
2023-07-09 01:49:54 -0400

    fixed all transform and extract methods

--- extract/fdic.py
+++ extract/fdic.py
@@ -4,15 +4,18 @@ import requests
 from requests.adapters import HTTPAdapter
 import time
 import aws_read_write
+import util
 
 # read credentials from the config file
 cfg_data = configparser.ConfigParser()
 cfg_data.read("keys_config.cfg")
 S3_BUCKET_NAME = cfg_data["S3"]["bucket_name"]
 
+SOURCE_NAME = 'fdic'
+
 ### START OF EXTRACT METHODS ###
 
-def extract_locations():
+def extract_location():
     s = requests.Session()
     s.mount("https://", HTTPAdapter(max_retries=10))
 
@@ -25,6 +28,8 @@ def extract_locations():
         locations = pd.concat([locations, pd.json_normalize(r.json()['data'])])
         offset += limit
         time.sleep(5)
+
+    locations.columns = [x.replace('data.', '') for x in locations.columns]
 
     return locations
 
@@ -93,45 +98,73 @@ def extract_institutions():
 
     return institutions
 
-
+# FIXME: institutions data does not get fed into any of our tables
 def load_raw_institutions():
     institutions = extract_institutions()
     institutions.columns = [x.replace('data.', '') for x in institutions.columns]
     institutions.to_csv('../../data/institutions.csv', index=False)
 
-
 def load_raw_financials():
-    financials_df = extract_financials()
-    # financials_df.to_parquet('../../data/financials.parquet') # FIXME: I don't think we need the paquet if we have the csv
-    financials_df.to_csv('../../data/financials.csv', index=False)
+    stock_prices = extract_financials()
+    dest_table_name = 'financials'
+    csv_file_name = SOURCE_NAME + dest_table_name + '.csv'
+    s3_object_name= 'raw_data/' + csv_file_name
 
-def load_raw_locations():
-    locations = extract_locations()
-    locations.columns = [x.replace('data.', '') for x in locations.columns]
-    locations.to_csv('../../data/raw_data/locations.csv', index=False)
+    util.load_raw_data(stock_prices, csv_file_name, s3_object_name)
 
+def load_raw_location():
+    stock_prices = extract_location()
+    dest_table_name = 'location'
+    csv_file_name = SOURCE_NAME + dest_table_name + '.csv'
+    s3_object_name= 'raw_data/' + csv_file_name
+
+    util.load_raw_data(stock_prices, csv_file_name, s3_object_name)
 
 ### END OF EXTRACT METHODS ###
 
 ### START OF TRANSFORM METHODS ###
 
-def transform_locations(): 
+def transform_location(): 
     # selected banks from FDIC
     CERT_LIST = [24735, 59017, 21761, 628, 29147, 27389, 3511, 5146, 18409, 33947, 7213, 3510, 34968, 57803]
 
-    locations_df = aws_read_write.get_csv(bucket_name=S3_BUCKET_NAME, object_name='raw_data/locations.csv')
+    location_df = aws_read_write.get_csv(bucket_name=S3_BUCKET_NAME, object_name='raw_data/location.csv')
 
-    clean_locations = locations_df[locations_df.CERT.isin(CERT_LIST)].sort_values('NAME')[['CERT','NAME','MAINOFF','OFFNAME','ESTYMD','SERVTYPE','ADDRESS','COUNTY','CITY','STNAME','ZIP','LATITUDE','LONGITUDE']].rename(columns={'NAME':'company_name', 'MAINOFF':'main_office', 'OFFNAME':'branch_name', 'ESTYMD':'established_date', 'SERVTYPE':'service_type', 'STNAME':'state'})
-    clean_locations.columns = [x.lower() for x in clean_locations.columns]
-    clean_locations['established_date'] = pd.to_datetime(clean_locations['established_date'])
-    clean_locations['service_type'] = clean_locations['service_type'].replace({11:'Full Service Brick and Mortar Office', 12:'Full Service Retail Office', 13:'Full Service Cyber Office', 14:'Full Service Mobile Office', 15:'Full Service Home/Phone Banking', 16:'Full Service Seasonal Office', 21:'Limited Service Administrative Office', 22:'Limited Service Military Facility', 23:'Limited Service Facility Office', 24:'Limited Service Loan Production Office', 25:'Limited Service Consumer Credit Office', 26:'Limited Service Contractual Office', 27:'Limited Service Messenger Office', 28:'Limited Service Retail Office', 29:'Limited Service Mobile Office', 30:'Limited Service Trust Office'})
-    clean_locations['zip'] = clean_locations['zip'].astype(str)
-    clean_locations.loc[~clean_locations.state.isin(['Puerto Rico','Virgin Islands Of The U.S.']) & clean_locations.zip.apply(lambda x: len(x)!=5), 'zip'] = clean_locations['zip'].str.zfill(5)
+    clean_location = location_df[location_df.CERT.isin(CERT_LIST)].sort_values('NAME')[['CERT','NAME','MAINOFF','OFFNAME','ESTYMD','SERVTYPE','ADDRESS','COUNTY','CITY','STNAME','ZIP','LATITUDE','LONGITUDE']].rename(columns={'NAME':'company_name', 'MAINOFF':'main_office', 'OFFNAME':'branch_name', 'ESTYMD':'established_date', 'SERVTYPE':'service_type', 'STNAME':'state'})
+    clean_location.columns = [x.lower() for x in clean_location.columns]
+    clean_location['established_date'] = pd.to_datetime(clean_location['established_date'])
+    clean_location['service_type'] = clean_location['service_type'].replace({11:'Full Service Brick and Mortar Office', 12:'Full Service Retail Office', 13:'Full Service Cyber Office', 14:'Full Service Mobile Office', 15:'Full Service Home/Phone Banking', 16:'Full Service Seasonal Office', 21:'Limited Service Administrative Office', 22:'Limited Service Military Facility', 23:'Limited Service Facility Office', 24:'Limited Service Loan Production Office', 25:'Limited Service Consumer Credit Office', 26:'Limited Service Contractual Office', 27:'Limited Service Messenger Office', 28:'Limited Service Retail Office', 29:'Limited Service Mobile Office', 30:'Limited Service Trust Office'})
+    clean_location['zip'] = clean_location['zip'].astype(str)
+    clean_location.loc[~clean_location.state.isin(['Puerto Rico','Virgin Islands Of The U.S.']) & clean_location.zip.apply(lambda x: len(x)!=5), 'zip'] = clean_location['zip'].str.zfill(5)
 
-def load_clean_locations():
-    clean_locations.to_csv(data_path + "\\clean_locations.csv", index=False)
-    aws_read_write.upload_file(file_name=data_path + '\\clean_locations.csv', bucket_name=S3_BUCKET_NAME, object_name='clean_data/locations.csv')
+    return clean_location
 
+def load_clean_location():
+
+    clean_data_path = 'location.csv'
+    existing_object_name='clean_data/location.csv'
+    clean_av_stock_price = transform_location()
+
+    util.load_clean_data(clean_av_stock_price, clean_data_path, existing_object_name)
+
+def transform_financials():
+
+    dest_table_name = 'financials'
+    csv_file_name = SOURCE_NAME + dest_table_name + '.csv'
+    raw_s3_object_name= 'raw_data/' + csv_file_name
+
+    financials_df = aws_read_write.get_csv(bucket_name=S3_BUCKET_NAME, object_name=raw_s3_object_name)
+
+    pass 
+
+
+def load_clean_financials():
+
+    clean_data_path = 'financials.csv'
+    existing_object_name='clean_data/financials.csv'
+    clean_av_stock_price = transform_financials()
+
+    util.load_clean_data(clean_av_stock_price, clean_data_path, existing_object_name)
 
 ### END OF TRANSFORM METHODS ###
 
