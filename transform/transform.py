30c3577ce817ec966230033dfb2e1a951cea0821
nalediym <67169576+nalediym@users.noreply.github.com>
2023-07-08 23:36:57 -0400

    added id cols to sql

--- transform/transform.py
+++ transform/transform.py
@@ -16,8 +16,8 @@ S3_BUCKET_NAME = cfg_data["S3"]["bucket_name"]
 TABLE_COLUMN_NAMES = {
     'company': ['id', 'name', 'symbolid', 'FDICCertID', 'Class', 'Status'],
     'debt_to_equity': ['ID', 'Date', 'LongTermDebt', 'DebtEquityRatio'],
-    'financials': ['report_date', 'total_assets', 'total_liabilities', 'total_debt', 'assets_return', 'equity_return', 'efficiency', 'risk_base_capital_ratio'],
-    'location': ['cert', 'company_name', 'main_office', 'branch_name', 'established_date', 'service_type', 'address', 'county', 'city', 'state', 'zip', 'latitude', 'longitude'],
+    'financials': ['ID', 'report_date', 'total_assets', 'total_liabilities', 'total_debt', 'assets_return', 'equity_return', 'efficiency', 'risk_base_capital_ratio'],
+    'location': ['ID', 'cert', 'company_name', 'main_office', 'branch_name', 'established_date', 'service_type', 'address', 'county', 'city', 'state', 'zip', 'latitude', 'longitude'],
     'price_history': ['symbol', 'date', 'open', 'high', 'low', 'close', 'adjusted_close', 'volume'],
     'sec_data': ['ID', 'asset_num', 'company_id', 'report_type', 'start_date', 'end_date', 'date_filed', 'fiscal_year', 'fiscal_period', 'form', 'frame', 'value'],
     'symbol': ['ID', 'TickerSymbol', 'Name'],
