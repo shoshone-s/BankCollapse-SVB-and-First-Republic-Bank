c8cd2c9c08624251db856d55a1527248a2e9f6ce
Angela <38588380+achen788@users.noreply.github.com>
2023-07-14 16:03:05 -0400

    transform scripts

---
+++ scripts/transform/company.py
@@ -0,0 +1,65 @@
+import pandas as pd
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).resolve().parents[1] / "utilities"))
+import util
+import aws_read_write
+
+def transform_fdic():
+    
+    dest_table_name = 'company'
+    source = 'fdic'
+    csv_file_name = source + '_' + dest_table_name + '.csv'
+    raw_s3_object_name= 'data/raw_data/' + csv_file_name
+    
+    # selected banks from FDIC
+    CERT_LIST = [24735, 59017, 21761, 628, 29147, 27389, 3511, 5146, 18409, 33947, 7213, 3510, 34968, 57803]
+   
+    institutions_df = aws_read_write.get_csv(bucket_name=util.S3_BUCKET_NAME, object_name=raw_s3_object_name)
+    institutions_subset = institutions_df[institutions_df.CERT.isin(CERT_LIST)].copy()
+    institutions_subset['Status'] = institutions_subset['ACTIVE'].replace({0:'closed or not insured by FDIC', 1:'currently open and insured by the FDIC'})
+    institutions_subset['Established Date'] = pd.to_datetime(institutions_subset['ESTYMD'])
+    institutions_subset['Category'] = institutions_subset['CLCODE'].replace({3:'National bank', 13:'State commercial bank', 21:'State commercial bank', 41:'State chartered stock savings and co-operative bank'})
+    institutions_subset['Symbol'] = list(map(lambda x: 'ALLY' if 'ally bank' in x else 'BAC' if 'bank of america' in x else 'C' if 'citibank' in x else 'JPM' if 'jpmorgan' in x else 'NECB' if 'northeast community bank' in x else 'BPOP' if 'banco popular' in x else 'TD' if 'td bank' in x else 'WFC' if 'wells fargo' in x else 'SIVB' if 'silicon valley bank' in x else 'FRC' if 'first republic bank' in x else np.nan, institutions_subset['NAME'].str.lower()))
+    
+    return institutions_subset
+
+
+def transform_alpha_vantage():
+    
+    dest_table_name = 'company'
+    source = 'alpha_vantage'
+    csv_file_name = source + '_' + dest_table_name + '.csv'
+    raw_s3_object_name= 'data/raw_data/' + csv_file_name
+    
+    av_companies = aws_read_write.get_csv(bucket_name=util.S3_BUCKET_NAME, object_name=raw_s3_object_name)
+    
+    return av_companies
+
+
+def transform_company():
+    fdic_df = transform_fdic()
+    av_df = transform_alpha_vantage()
+    
+    # combine data from FDIC and Alpha Vantage
+
+    clean_companies = fdic_df.rename(columns={'CERT':'FDIC_CERT_ID'})[['Symbol','FDIC_CERT_ID','NAME','Category','Status','Established Date']].merge(av_df[['Symbol','Exchange','Sector']], on='Symbol', how='outer')
+    clean_companies.columns = [x.lower().replace(' ','_') for x in clean_companies.columns]
+
+    return clean_companies
+    
+
+def load_clean_company():    
+    clean_companies = transform_company()
+    dest_table_name = 'company'
+    csv_file_name = dest_table_name + '.csv'
+    util.load_clean_data(clean_companies, csv_file_name)
+    
+    
+def transform(table_name='all'):
+    if table_name == 'all':
+        load_clean_company()
+    elif table_name == 'company':
+        load_clean_company()
+    else:
+        print('Invalid table name')
---
+++ scripts/transform/price_history.py
@@ -0,0 +1,89 @@
+import pandas as pd
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).resolve().parents[1] / "utilities"))
+import util
+import aws_read_write
+
+def transform_alpha_vantage():
+    
+    dest_table_name = 'price_history'
+    source = 'alpha_vantage'
+    csv_file_name = source + '_' + dest_table_name + '.csv'
+    raw_s3_object_name= 'data/raw_data/' + csv_file_name
+    
+    av_stock_price = aws_read_write.get_csv(bucket_name=util.S3_BUCKET_NAME, object_name=raw_s3_object_name)
+    av_stock_price.columns = [x.lower().replace(' ','_') for x in av_stock_price.columns]
+    av_stock_price['date'] = pd.to_datetime(av_stock_price['date'])
+
+    # keep stock data from Jan 2017 to Mar 2022
+    MIN_DATE = pd.Timestamp(2017,1,1)
+    MAX_DATE = pd.Timestamp(2022,3,31)
+    clean_av_stock_price = av_stock_price[['symbol', 'date', 'open', 'high', 'low', 'close', 'adjusted_close', 'volume']]
+    clean_av_stock_price = clean_av_stock_price[(clean_av_stock_price.date>=MIN_DATE) & (clean_av_stock_price.date<=MAX_DATE)]
+    clean_av_stock_price['volume'] = clean_av_stock_price['volume'].astype('Int64')
+
+    return clean_av_stock_price
+
+def transform_market_watch():
+    dest_table_name = 'price_history'
+    source = 'market_watch'
+    csv_file_name = source + '_' + dest_table_name + '.csv'
+    raw_s3_object_name= 'data/raw_data/' + csv_file_name
+    
+    djusbank = aws_read_write.get_csv(bucket_name=util.S3_BUCKET_NAME, object_name=raw_s3_object_name)
+    djusbank.columns = [x.lower() for x in djusbank.columns]
+    djusbank.rename(columns={'ticker':'symbol'}, inplace=True)
+    djusbank['date'] = pd.to_datetime(djusbank['date']) 
+    
+    # keep stock data from Jan 2017 to Mar 2022
+    MIN_DATE = pd.Timestamp(2017,1,1)
+    MAX_DATE = pd.Timestamp(2022,3,31)
+    djusbank = djusbank[['symbol', 'date', 'open', 'high', 'low', 'close']] # 'adjusted_close', 'volume' removed from site
+    djusbank = djusbank[(djusbank.date>=MIN_DATE) & (djusbank.date<=MAX_DATE)]
+    # djusbank['volume'] = djusbank['volume'].astype('Int64')
+
+    return djusbank
+
+def transform_y_finance():
+    dest_table_name = 'price_history'
+    source = 'yfinance'
+    csv_file_name = source + '_' + dest_table_name + '.csv'
+    raw_s3_object_name= 'data/raw_data/' + csv_file_name
+    
+    yf_stock_price = aws_read_write.get_csv(bucket_name=util.S3_BUCKET_NAME, object_name=raw_s3_object_name).drop(columns=['Unnamed: 0'])
+    yf_stock_price.columns = [x.lower().replace(' ','_') for x in yf_stock_price.columns]
+    yf_stock_price.rename(columns={'datetime':'date', 'adj_close':'adjusted_close'}, inplace=True)
+    yf_stock_price['symbol'] = 'SIVBQ'
+    yf_stock_price['date'] = pd.to_datetime(yf_stock_price['date']) 
+
+    # keep stock data from Jan 2017 to Mar 2022
+    MIN_DATE = pd.Timestamp(2017,1,1)
+    MAX_DATE = pd.Timestamp(2022,3,31)
+    yf_stock_price = yf_stock_price[['symbol', 'date', 'open', 'high', 'low', 'close', 'adjusted_close', 'volume']]
+    yf_stock_price = yf_stock_price[(yf_stock_price.date>=MIN_DATE) & (yf_stock_price.date<=MAX_DATE)]
+    yf_stock_price['volume'] = yf_stock_price['volume'].astype('Int64')
+
+    return yf_stock_price
+    
+def transform_price_history():
+    av_df = transform_alpha_vantage()
+    mw_df = transform_market_watch()
+    yf_df = transform_y_finance()
+
+    return pd.concat([av_df, mw_df, yf_df])
+
+def load_clean_price_history():    
+    clean_av_stock_price = transform_price_history()
+    dest_table_name = 'price_history'
+    csv_file_name = dest_table_name + '.csv'
+    util.load_clean_data(clean_av_stock_price, csv_file_name)
+    
+    
+def transform(table_name='all'):
+    if table_name == 'all':
+        load_clean_price_history()
+    elif table_name == 'price_history':
+        load_clean_price_history()
+    else:
+        print('Invalid table name')
