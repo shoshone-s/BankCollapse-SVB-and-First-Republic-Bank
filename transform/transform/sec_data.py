da933b6438817128e29f847cf9d3f808d807dde3
shoshone-s <shoshone.y.smith@gmail.com>
2023-07-14 16:16:18 -0300

    updated scripts

--- transform/transform/sec_data.py
+++ transform/transform/sec_data.py
@@ -1,15 +1,24 @@
 # import util
 # import aws_read_write
 
-# cleaning up the sec data
-# - ie. this should drop data outside of the 2017-2022 dates
-def clean_sec_data():
-    dest_table_name = 'sec_data'
-    source = 'sec_data'
-    csv_file_name = source + '_' + dest_table_name + '.csv'
-    raw_s3_object_name= 'data/raw_data/' + csv_file_name
+# import csv
+# from pprint import pprint
 
-    # sec_df = aws_read_write.get_csv(bucket_name=util.S3_BUCKET_NAME, object_name=raw_s3_object_name)
+# # cleaning up the sec data
+# # - ie. this should drop data outside of the 2017-2022 dates
+# def clean_sec_data():
+#     dest_table_name = 'sec_data'
+#     source = 'sec_data'
+#     csv_file_name = source + '_' + dest_table_name + '.csv'
+#     raw_s3_object_name= 'data/raw_data/' + csv_file_name
+
+#     # sec_df = aws_read_write.get_csv(bucket_name=util.S3_BUCKET_NAME, object_name=raw_s3_object_name)
+#     with open('../../data/secData.csv', 'r') as readerFile:
+#         csvreader = csv.reader(readerFile)
+#         for row in csvreader:
+#             pprint(type(row[0]))
+
+# clean_sec_data()
 
 
 
